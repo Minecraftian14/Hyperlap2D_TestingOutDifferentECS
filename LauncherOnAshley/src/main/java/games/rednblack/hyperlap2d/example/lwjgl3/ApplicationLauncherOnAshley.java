e6c7ac51fd5f66b3fb1cce544b999f566ba3a3e6
MCXIV <mcxiv14@gmail.com>
2021-06-13 19:18:52 +0530

    oof

--- LauncherOnAshley/src/main/java/games/rednblack/hyperlap2d/example/lwjgl3/ApplicationLauncherOnAshley.java
+++ LauncherOnAshley/src/main/java/games/rednblack/hyperlap2d/example/lwjgl3/ApplicationLauncherOnAshley.java
@@ -1,12 +1,19 @@
 package games.rednblack.hyperlap2d.example.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import games.rednblack.hyperlap2d.example.HyperLap2DExampleOnAshley;
-
-import static games.rednblack.hyperlap2d.example.lwjgl3.Lwjgl3Launcher.getDefaultConfiguration;
 
 public class ApplicationLauncherOnAshley {
     public static void main(String[] args) {
         new Lwjgl3Application(new HyperLap2DExampleOnAshley(), getDefaultConfiguration("Ashley"));
+    }
+
+    public static Lwjgl3ApplicationConfiguration getDefaultConfiguration(String name) {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("HyperLap2DExample:" + name);
+        configuration.setWindowedMode(640, 480);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
     }
 }
